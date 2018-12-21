e863364d2fbe5aca52cdfade1678a09c740922f5
IkarosCoCo <zhouke_deem@qq.com>
2018-12-21 11:35:42 +0800

    add tps data statistics

--- cmd/main.go
+++ cmd/main.go
@@ -1,23 +1,26 @@
 package main
 
 import (
-	"math/big"
 	"fmt"
 	"github.com/usechain/go-usedrpc"
+	"math/big"
+	"time"
 )
 
 func main() {
 	c := usedrpc.NewUseRPC("http://127.0.0.1:8545")
 	accounts, _ := c.UseAccounts()
 	tx := usedrpc.T {
-		From: accounts[0],
-		To:   "0xfffffffffffffffffffffffffffffffff0000001",
+		From: accounts[1],
+		To:   "0xfffffffffffffffffffffffffffffffff0000008",
 		Value: big.NewInt(0),
 		Data:  "",
+		GasPrice: big.NewInt(1000000000000000000), //10^18
 	}
 	for {
 		flag, err := c.UseSendTransaction(tx)
 		fmt.Println("The tx hash:", err, flag)
+		time.Sleep(50*time.Millisecond)
 	}
 }
 
---
+++ cmd/tps.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"github.com/usechain/go-usedrpc"
+	"os"
+	"strconv"
+)
+
+func main() {
+	start, _ := strconv.Atoi(os.Args[1])
+	end, _ := strconv.Atoi(os.Args[2])
+	var slot = end - start
+	c := usedrpc.NewUseRPC("http://127.0.0.1:8545")
+	blockStart, _ := c.UseGetBlockByNumber(start, false)
+	blockNow, _ := c.UseGetBlockByNumber(end, false)
+	timeslot := blockNow.Timestamp - blockStart.Timestamp
+	var totalTxNum = 0
+	var totalSize = 0
+	var maxSize = 0
+	var maxslot = 0
+	var minerNum = make(map[string]int)
+
+	for  i := start; i < end; i++ {
+		num, _ := c.UseGetBlockTransactionCountByNumber(i)
+		totalTxNum += num
+		blockbefore, _ := c.UseGetBlockByNumber(i,false)
+		block, _ := c.UseGetBlockByNumber(i+1,false)
+		totalSize += block.Size
+		if maxSize < block.Size{
+			maxSize = block.Size
+		}
+		tempslot := block.Timestamp - blockbefore.Timestamp
+		if maxslot < tempslot{
+			maxslot = tempslot
+		}
+
+		minerNum[blockbefore.Miner]++
+	}
+	fmt.Println("总交易数量：", totalTxNum)
+	fmt.Println("总耗时(s)：", timeslot)
+	fmt.Println("tps：", totalTxNum/timeslot)
+	fmt.Println("平均出块延时(s)：", timeslot/slot)
+	fmt.Println("最大出块延时(s)：", maxslot)
+	fmt.Println("平均区块大小(byte)：", totalSize/slot)
+	fmt.Println("最大区块大小(byte)：", maxSize)
+	fmt.Println("平均每个区块包含交易数量：", totalTxNum/slot)
+	for minernum := range minerNum {
+		fmt.Println(minernum,"在此期间共产出",minerNum[minernum],"个区块")
+	}
+}
