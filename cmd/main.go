5d93c615577701919cd271cb2f8532fc189da555
IkarosCoCo <zhouke_deem@qq.com>
2019-03-23 10:23:12 +0800

    add random address and value

--- cmd/main.go
+++ cmd/main.go
@@ -4,25 +4,33 @@ import (
 	"fmt"
 	"github.com/usechain/go-usedrpc"
 	"math/big"
+	"math/rand"
 	"time"
 )
 
-func main() {
-	c := usedrpc.NewUseRPC("http://127.0.0.1:8545")
-	accounts, _ := c.UseAccounts()
-	tx := usedrpc.T {
-		From: accounts[1],
-		To:   "0xfffffffffffffffffffffffffffffffff0000008",
-		Value: big.NewInt(0),
-		Data:  "",
-		GasPrice: big.NewInt(1000000000000000000), //10^18
+var letters = []rune("0123456789abcdef")
+
+func randSeq(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
 	}
-	for {
-		flag, err := c.UseSendTransaction(tx)
-		fmt.Println("The tx hash:", err, flag)
-		time.Sleep(50*time.Millisecond)
-	}
+	return string(b)
 }
 
-
-
+func main() {
+	c := usedrpc.NewUseRPC("http://127.0.0.1:8848")
+	accounts, _ := c.UseAccounts()
+	for {
+		tx := usedrpc.T{
+			From:     accounts[0],
+			To:       "0x" + randSeq(40),
+			Value:    big.NewInt(int64(rand.Intn(10000000000000000))),
+			Data:     "",
+			GasPrice: big.NewInt(40000000000), //10^10
+		}
+		flag, err := c.UseSendTransaction(tx)
+		fmt.Println("The tx hash:", err, flag)
+		time.Sleep(50 * time.Millisecond)
+	}
+}
