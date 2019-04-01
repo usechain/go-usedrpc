c70a80270cbab8143e0366d5841566e4a3d1d0e8
IkarosCoCo <zhouke_deem@qq.com>
2019-04-01 10:59:02 +0800

    fix bug

--- cmd/block_monitor.go
+++ cmd/block_monitor.go
@@ -9,23 +9,23 @@ import (
 func main() {
 	for {
 		c := usedrpc.NewUseRPC("http://127.0.0.1:8848")
-		lastBlockNumberBefore, _ := c.UseBlockNumber()
+		lastBlockNumberBefore, err := c.UseBlockNumber()
+		if err != nil {
+			sendEmail("./mail_Browser.sh")
+		}
 		time.Sleep(10 * time.Minute)
-		lastBlockNumberNow, _ := c.UseBlockNumber()
+		lastBlockNumberNow, err := c.UseBlockNumber()
+		if err != nil {
+			sendEmail("./mail_Browser.sh")
+		}
 		if lastBlockNumberBefore == lastBlockNumberNow {
-			sendEmail()
+			sendEmail("./mail.sh")
 			return
 		}
 	}
 }
 
-func sendEmail() {
-	cmd := exec.Command("./mail.sh")
-
-	err := cmd.Run()
-	if err != nil {
-		//fmt.Println("Execute Command failed:" + err.Error())
-		return
-	}
-	//fmt.Println("Execute Command finished.")
+func sendEmail(sh string) {
+	cmd := exec.Command(sh)
+	cmd.Run()
 }
---
+++ cmd/mail_Browser.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+echo "No blocks have appeared in the past 10 minutes"|mail -s "usechain mainnet error" lemengbin@163.com,lyszhang@126.com,15989559626@163.com,liningning901@163.com,yzzjwzk@163.com
