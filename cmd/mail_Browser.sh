222942542fe2e0a2c0342234cff053ab726ad39f
IkarosCoCo <zhouke_deem@qq.com>
2019-04-01 11:01:04 +0800

    fix bug

--- cmd/mail_Browser.sh
+++ cmd/mail_Browser.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo "No blocks have appeared in the past 10 minutes"|mail -s "usechain mainnet error" lemengbin@163.com,lyszhang@126.com,15989559626@163.com,liningning901@163.com,yzzjwzk@163.com
+echo "Can't get Block number from 39.105.89.191, Browser problem ?"|mail -s "usechain mainnet error" lemengbin@163.com,lyszhang@126.com,15989559626@163.com,liningning901@163.com,yzzjwzk@163.com
